281f54cb15e57c574e07413ffbd9bef387457f6c
Anchal Chaurasiya <anchalchaurasiya123@outlook.com>
2025-01-08 21:34:38 +0530

    commit

---
+++ BAL/ProductCategoryService.cs
@@ -0,0 +1,108 @@
+﻿using Dapper;
+using MyApp.Models.Common;
+using MyApp.Models;
+using System.Collections.Generic;
+using System;
+
+namespace MyApp.BAL
+{
+    public static class ProductCategoryService
+    {
+        public static List<ProductCategoryDto> GetProductCategoryList()
+        {
+            List<ProductCategoryDto> res = new List<ProductCategoryDto>();
+            try
+            {
+                string _proc = "Proc_ProductCategory";
+                var queryparameter = new DynamicParameters();
+                queryparameter.Add("@ProcId", 2);
+                res = DBHelperDapper.GetAllModelList<ProductCategoryDto>(_proc, queryparameter);
+            }
+            catch (Exception ex) { }
+            return res;
+        }
+
+        public static CommonResponseDto SaveProductCategory(ProductCategoryDto productCategory)
+        {
+            CommonResponseDto res = new CommonResponseDto();
+            try
+            {
+                string _proc = "Proc_ProductCategory";
+                var queryparameter = new DynamicParameters();
+                queryparameter.Add("@ProcId", 1);
+                queryparameter.Add("@PrdCatCode", productCategory.ProductCategoryCode);
+                queryparameter.Add("@ZMGRP", productCategory.ZMGRP);
+                queryparameter.Add("@ZSGRP1", productCategory.ZSGRP1);
+                queryparameter.Add("@ZSGRP2", productCategory.ZSGRP2);
+                queryparameter.Add("@ZSGRP3", productCategory.ZSGRP3);
+                queryparameter.Add("@ZSGRP4", productCategory.ZSGRP4);
+                queryparameter.Add("@ProductType", productCategory.ProductType);
+                queryparameter.Add("@PackContains", productCategory.PackContains);
+                queryparameter.Add("@ConversionRatio", productCategory.ConversionRatio);
+                queryparameter.Add("@createdBy", SessionManager.UserId);
+                queryparameter.Add("@IsActive", productCategory.IsActive);
+                queryparameter.Add("@createdBy", SessionManager.UserId);
+                CommonFunction.Printparameter(queryparameter, "Product Category paramer for saving:");//FOR WRITE LOG'
+                res = DBHelperDapper.GetAllModel<CommonResponseDto>(_proc, queryparameter);
+            }
+            catch (Exception ex) { }
+            return res;
+        }
+        public static ProductCategoryDto GetProductCategory(string PrdCatGuid)
+        {
+            ProductCategoryDto res = new ProductCategoryDto();
+            try
+            {
+                string _proc = "Proc_ProductCategory";
+                var queryparameter = new DynamicParameters();
+                queryparameter.Add("@ProcId", 3);
+                queryparameter.Add("@PrdCatGuid", PrdCatGuid);
+                CommonFunction.Printparameter(queryparameter, "Get Product Category:");//FOR WRITE LOG'
+                res = DBHelperDapper.GetAllModel<ProductCategoryDto>(_proc, queryparameter);
+            }
+            catch (Exception ex) { }
+            return res;
+        }
+        public static CommonResponseDto UpdateProductCategory(ProductCategoryDto productCategory)
+        {
+            CommonResponseDto res = new CommonResponseDto();
+            try
+            {
+                string _proc = "Proc_ProductCategory";
+                var queryparameter = new DynamicParameters();
+                queryparameter.Add("@ProcId", 4);
+                queryparameter.Add("@PrdCatCode", productCategory.ProductCategoryCode);
+                queryparameter.Add("@ZMGRP", productCategory.ZMGRP);
+                queryparameter.Add("@ZSGRP1", productCategory.ZSGRP1);
+                queryparameter.Add("@ZSGRP2", productCategory.ZSGRP2);
+                queryparameter.Add("@ZSGRP3", productCategory.ZSGRP3);
+                queryparameter.Add("@ZSGRP4", productCategory.ZSGRP4);
+                queryparameter.Add("@ProductType", productCategory.ProductType);
+                queryparameter.Add("@PackContains", productCategory.PackContains);
+                queryparameter.Add("@ConversionRatio", productCategory.ConversionRatio);
+                queryparameter.Add("@ModifiedBy", SessionManager.UserId);
+                queryparameter.Add("@IsActive", productCategory.IsActive);
+                queryparameter.Add("@PrdCatGuid", productCategory.ProductCategoryGuid);
+                CommonFunction.Printparameter(queryparameter, "Product Category paramer for updating:");//FOR WRITE LOG'
+                res = DBHelperDapper.GetAllModel<CommonResponseDto>(_proc, queryparameter);
+            }
+            catch (Exception ex) { }
+            return res;
+        }
+
+        public static CommonResponseDto CheckProductCategory(long productcategorycode)
+        {
+            CommonResponseDto res = new CommonResponseDto();
+            try
+            {
+                string _proc = "Proc_ProductCategory";
+                var queryparameter = new DynamicParameters();
+                queryparameter.Add("@ProcId", 5);
+                queryparameter.Add("@PrdCatCode", productcategorycode);
+                res = DBHelperDapper.GetAllModel<CommonResponseDto>(_proc, queryparameter);
+            }
+            catch (Exception ex) { }
+            return res;
+        }
+    }
+}
---
+++ Controllers/ProductCategoryController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MyApp.BAL;
+using MyApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using MyApp.Models.Base;
+using X.PagedList.Extensions;
+
+namespace MyApp.Controllers
+{
+    [CustomAuthenticationFilter]
+    public class ProductCategoryController : BaseController
+    {
+        public ProductCategoryController(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+
+        [HttpGet]
+        public IActionResult Save(string? id)
+        {
+            ProductCategoryDto res = new ProductCategoryDto();
+            ViewBag.Button = "Submit";
+            res.IsActive = true;
+            if (id != null)
+            {
+                res = ProductCategoryService.GetProductCategory(id);
+                ViewBag.Button = "Update";
+            }
+            return View(res);
+        }
+
+        public JsonResult Save([FromBody] ProductCategoryDto productCategory)
+        {
+            var result = (CommonResponseDto?)null;
+            if (productCategory.ProductCategoryGuid == Guid.Empty)
+            {
+                result = ProductCategoryService.CheckProductCategory(productCategory.ProductCategoryCode);
+                if (result != null) 
+                {
+                    return Json(result);
+                }
+            }
+            if (productCategory.ProductCategoryGuid == Guid.Empty)
+            {
+                result = ProductCategoryService.SaveProductCategory(productCategory);
+            }
+            else
+            {
+                result = ProductCategoryService.UpdateProductCategory(productCategory);
+            }
+
+            return Json(result);
+        }
+        [HttpGet]
+        public IActionResult Index(int? page)
+        {
+
+            List<ProductCategoryDto> res = ProductCategoryService.GetProductCategoryList();
+            return View(res.ToPagedList(page ?? 1, PageRecordCount));
+        }
+        public IActionResult GetPagedProductCategory(int page, int rowperpage, string prdcatcode, string producttype, string zmgrp)
+        {
+            var pageSize = rowperpage != 0 ? rowperpage : PageRecordCount;
+            var res = ProductCategoryService.GetProductCategoryList();
+
+            var filters = new Dictionary<string, string>
+                   {
+                     { "ProductCategoryCode", prdcatcode },
+                     { "ProductType", producttype },
+                     { "ZMGRP",zmgrp }
+                   };
+            IQueryable<ProductCategoryDto> query = res.AsQueryable(); // employeeList is your data source
+            query = query.ApplyFilters(filters);
+            return PartialView("_ProductCategoryTable", query.ToPagedList(page, pageSize));
+
+        }
+
+        public IActionResult CheckProductCategoryCode(long ProductCategoryCode)
+        {
+            CommonResponseDto res = ProductCategoryService.CheckProductCategory(ProductCategoryCode);
+            return Json(res);
+        }
+    }
+}
